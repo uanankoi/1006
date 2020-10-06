4dc95f09ad5d8d4dd2413ce5f760801a0754d709
uanankoi <64210229+uanankoi@users.noreply.github.com>
2020-10-06 12:49:24 +0800

    main更新

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          REACT GITHUBPAGES 完成啦!可以code了!
         </p>
         <a
           className="App-link"
